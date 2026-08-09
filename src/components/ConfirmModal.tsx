73e608b11c32d683d98591cab40aa59f2ca60299
lighminge <lighminge@gmail.com>
2026-08-09 15:16:27 +0800

    Fix ConfirmModal z-index to appear above other modals

--- src/components/ConfirmModal.tsx
+++ src/components/ConfirmModal.tsx
@@ -17,7 +17,7 @@ export default function ConfirmModal({
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-[100] px-4">
+        <div className="fixed inset-0 flex items-center justify-center z-[1000] px-4">
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
